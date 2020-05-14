80d58a8d576816aa38ac0d3ae5b8a986c8465528
jae <utopia.jae@gmail.com>
2020-05-14 12:03:18 +0800

    initial

---
+++ main.js
@@ -0,0 +1,98 @@
+Vue.component('crud-item', {
+    props: {
+        crud: Object,
+        index: Number
+    },
+    template: `
+        <tr>
+        <th>{{crud.name}}</th>
+        <th>{{crud.age}}</th>
+        <th>{{crud.sex}}</th>
+        <th><button @click="editItem(index)">修改</button></th>
+        <th><button @click="removeItem(index)">刪除</button></th>
+        </tr>
+    `,
+    methods: {
+        removeItem(index) {
+            app.items.splice(index, 1);
+            console.log('刪除');
+        },
+        editItem(index) {
+            console.log(index);
+            let target = app.items[index];
+            app.type = 'confirm';
+            app.name = target.name;
+            app.age = target.age;
+            app.sex = target.sex;
+            app.target = index;
+        }
+    }
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        type: 'create',
+        target: null,
+        name: '',
+        age: '',
+        sex: '男',
+        items: [{
+                name: 'jae',
+                age: 10,
+                sex: '男'
+            },
+            {
+                name: 'ollie',
+                age: 11,
+                sex: '女'
+            },
+            {
+                name: 'frank',
+                age: 12,
+                sex: '男'
+            },
+            {
+                name: 'jimmy',
+                age: 13,
+                sex: '男'
+            },
+            {
+                name: 'geng',
+                age: 14,
+                sex: '男'
+            },
+            {
+                name: 'ivan',
+                age: 15,
+                sex: '男'
+            }
+        ]
+    },
+    methods: {
+        initInput() {
+            this.name = '';
+            this.age = '';
+            this.sex = '男';
+        },
+        operation() {
+            if (this.type === 'create') {
+                if (!this.name || !this.age) return;
+                this.items.push({
+                    name: this.name,
+                    age: this.age,
+                    sex: this.sex
+                });
+                this.initInput();
+            } else {
+                let target = this.items[this.target];
+                target.name = this.name;
+                target.age = this.age;
+                target.sex = this.sex;
+                this.initInput();
+                this.target = null;
+                this.type = 'create';
+            }
+        }
+    }
+})
